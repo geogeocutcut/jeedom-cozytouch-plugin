8434892b43291d68c5be0ab4256962c8be1cf3e8
geogeocutcut <lefevregeoffrey@gmail.com>
2020-03-22 16:46:59 +0100

    replace object => jeeObject

--- desktop/php/cozytouch.php
+++ desktop/php/cozytouch.php
@@ -87,7 +87,7 @@ $eqLogics = eqLogic::byType('cozytouch');
                                 <select class="form-control eqLogicAttr" data-l1key="object_id">
                                     <option value="">{{Aucun}}</option>
                                     <?php
-                                    foreach (object::all() as $object) {
+                                    foreach (jeeObject::all() as $object) {
                                         echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
                                     }
                                     ?>
